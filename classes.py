19f3eaaf76abdcb973257cf3e0b296c7249355c9
harrycheese5 <12645@edu.wheatlandhigh.org>
2026-04-15 13:10:06 -0800

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,14 @@
+#Michael
+#Period 6
+#week 11
+#Time spent
+class Student:
+  def __init__(self, name):
+    self.name = name
+    self.grade = None
+
+    def set_grade(self, grade): 
+      self.grade = grade
+
+    def display_info(self)
+      print(f"Student {self.name), Grade: {self.grade}")
